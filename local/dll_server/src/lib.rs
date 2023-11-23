9edc19d2057dd7dcb793dff901b88f17031a9372
Cory Finger <cory.j.finger@gmail.com>
2023-11-23 15:24:23 -0800

    Read-write loop

--- local/dll_server/src/lib.rs
+++ local/dll_server/src/lib.rs
@@ -3,9 +3,55 @@ use std::net::{TcpListener, TcpStream};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
 use std::time::Duration;
+use std::io::{Error, ErrorKind, Read, Write};
 
-fn handle_client(stream: TcpStream) {
-    // ...
+const MESSAGE_SIZE: usize = 1;
+
+fn read_message(stream: &mut TcpStream) -> Result<String, std::string::FromUtf8Error> {
+    // Store all the bytes for our received String
+    let mut received: Vec<u8> = vec![];
+
+    // Array with a fixed size
+    let mut rx_bytes = [0u8; MESSAGE_SIZE];
+    loop {
+        // Read from the current data in the TcpStream
+        let bytes_read = stream.read(&mut rx_bytes).expect("Failed to read stream");
+        let recv_len = received.len();
+
+        if rx_bytes == "\n".as_bytes() {
+            break;
+        }
+
+        // However many bytes we read, extend the `received` string bytes
+        received.extend_from_slice(&rx_bytes[..bytes_read]);
+
+        // If we didn't fill the array
+        // stop reading because there's no more data (we hope!)
+        if bytes_read < MESSAGE_SIZE {
+            break;
+        }
+    }
+
+    String::from_utf8(received)
+}
+
+fn handle_client(mut stream: TcpStream) {
+    loop {
+        let message = read_message(&mut stream).expect("Could not parse message");
+
+        match message.trim() {
+            "quit" => {
+                println!("Client disconnected...");
+                break;
+            }
+
+            "get_players" => {
+                stream.write(b"42\n").expect("Tried to write to TCP Stream");
+            }
+
+            msg => println!("Unknown Message: {msg}")
+        }
+    }
 }
 
 fn listen_for_connections() {
@@ -20,7 +66,7 @@ fn listen_for_connections() {
         match stream {
             Ok(stream) => handle_client(stream),
             _ => ()
-        }
+        };
     }
 }
 
--- local/simulator/src/main.rs
+++ local/simulator/src/main.rs
@@ -3,14 +3,61 @@ use std::net::{TcpListener, TcpStream};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
 use std::time::Duration;
+use std::io::{Error, ErrorKind, Read, Write};
 
-fn handle_client(stream: TcpStream) {
-    // ...
+const MESSAGE_SIZE: usize = 1;
+
+fn read_message(stream: &mut TcpStream) -> Result<String, std::string::FromUtf8Error> {
+    // Store all the bytes for our received String
+    let mut received: Vec<u8> = vec![];
+
+    // Array with a fixed size
+    let mut rx_bytes = [0u8; MESSAGE_SIZE];
+    loop {
+        // Read from the current data in the TcpStream
+        let bytes_read = stream.read(&mut rx_bytes).expect("Failed to read stream");
+        let recv_len = received.len();
+
+        if rx_bytes == "\n".as_bytes() {
+            break;
+        }
+
+        // However many bytes we read, extend the `received` string bytes
+        received.extend_from_slice(&rx_bytes[..bytes_read]);
+
+        // If we didn't fill the array
+        // stop reading because there's no more data (we hope!)
+        if bytes_read < MESSAGE_SIZE {
+            break;
+        }
+    }
+
+    String::from_utf8(received)
+}
+
+fn handle_client(mut stream: TcpStream) {
+    loop {
+        let message = read_message(&mut stream).expect("Could not parse message");
+
+        match message.trim() {
+            "quit" => {
+                println!("Client disconnected...");
+                break;
+            }
+
+            "get_players" => {
+                stream.write(b"42\n").expect("Tried to write to TCP Stream");
+            }
+
+            msg => println!("Unknown Message: {msg}")
+        }
+    }
 }
 
 fn listen_for_connections() {
+    println!("Starting TCP Listener...");
+    let listener = TcpListener::bind("0.0.0.0:4951").expect("Could not open TCP Port");
     println!("Waiting for TCP Connections...");
-    let listener = TcpListener::bind("127.0.0.1:4951").expect("Could not open TCP Port");
 
     // accept connections and process them serially
     for stream in listener.incoming() {
@@ -19,8 +66,7 @@ fn listen_for_connections() {
         match stream {
             Ok(stream) => handle_client(stream),
             _ => ()
-        }
-        
+        };
     }
 }
 
