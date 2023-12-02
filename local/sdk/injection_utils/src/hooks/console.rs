c9ed7429a1b74f68b9074e044e023ebe6d8c48f1
Cory Finger <cory.j.finger@gmail.com>
2023-12-02 14:52:37 -0800

    Tweak how console commands are intercepted

--- local/sdk/injection_utils/src/hooks/console.rs
+++ local/sdk/injection_utils/src/hooks/console.rs
@@ -1,113 +1,45 @@
 use retour::static_detour;
-use ue_types::{FString, UConsole, UnknownType, UObject};
+use ue_types::{FString, UConsole};
 use game_base::GameBase;
-use utils::{logln, debug, warning};
-use std::sync::mpsc::{Sender, Receiver};
-use std::sync::mpsc;
+use utils::{debug, warning};
 
 static_detour! {
-    static ConsoleCommand: fn(*const UConsole, *const FString);
+    static CommandIntercept: fn(*const UConsole, *const FString);
 }
 
-#[derive(Debug, Clone)]
-pub enum UserCommand {
-    FindWithVFTable(*const *const UnknownType),
-    UnknownCommand(String),
-    CloseClient,
-    Noop
-}
-
-static mut CMD_CHANNEL: Option<Sender<UserCommand>> = None;
-
-pub fn initialize_shivs() -> Receiver<UserCommand> {
-    let game_base = GameBase::singleton();
-
-    let (tx, rx): (Sender<UserCommand>, Receiver<UserCommand>) = mpsc::channel();
-    unsafe { CMD_CHANNEL = Some(tx) };
-    
-    match game_base.console() {
-        Some(console) => unsafe {
-            let placed_shiv = ConsoleCommand.initialize(console._console_command(), console_command_shiv);
-            if placed_shiv.is_ok() {
-                ConsoleCommand.enable().expect("Failed enabling UConsole shiv!");
-                debug!("Successfully placed Console Shiv!");
-            } else {
-                warning!("Could not place Console Shiv!");
+type CommandInterceptShiv = fn(&UConsole, &FString) -> Result<bool, Box<dyn std::error::Error>>;
+pub fn add_command_intercept(shiv_fn: CommandInterceptShiv) -> Result<(), Box<dyn std::error::Error>> {
+    match GameBase::singleton().console() {
+        Some(console) => {
+            unsafe {
+                CommandIntercept.initialize(console._console_command(), call_intercept_shiv(shiv_fn))?;
+                CommandIntercept.enable()?;
             }
-        }
 
-        None => ()
-    }
-
-    rx
-}
-
-fn console_command_shiv(console_ptr: *const UConsole, cmd_ptr: *const FString) {
-    let cmd = unsafe { cmd_ptr.as_ref::<'static>().unwrap() };
-
-    let mut cmd_str = cmd.to_string();
-    cmd_str.truncate(cmd_str.len()-1);
-
-    let user_cmd = to_user_cmd(&cmd_str);
-    let tx = unsafe {
-        if CMD_CHANNEL.is_some() { Some(CMD_CHANNEL.as_ref().unwrap().clone()) } else { None }
-    };
-
-    println!("Detected console command: {} - {:?}", cmd_str, user_cmd);
-    use UserCommand::*;
-    match user_cmd {
-        UnknownCommand(_) => ConsoleCommand.call(console_ptr, cmd_ptr),
-        FindWithVFTable(table_addr) => find_with_vf_table(table_addr),
-        CloseClient => {
-            debug!("Told to close client! Exiting!");
-            std::process::exit(1)
+            debug!("[{}]: Intercepting in-game console commands!", GameBase::singleton().mod_name);
+            Ok(())
         },
-        _ => if tx.is_some() { tx.unwrap().send(user_cmd).unwrap() }
-    };
-}
 
-fn to_user_cmd(cmd_str: &String) -> UserCommand {
-    match (cmd_str.as_str(), cmd_str.split_once(" ")) {
-        ("close_client", _) => UserCommand::CloseClient,
-        ("noop", _) => UserCommand::Noop,
-        (_, Some(("vftable", args))) => {
-            let without_prefix = args.trim_start_matches("0x");
-
-            match usize::from_str_radix(without_prefix, 16) {
-                Ok(num) => UserCommand::FindWithVFTable(num as *const *const UnknownType),
-                _ => UserCommand::UnknownCommand(cmd_str.clone())
-            }
-            
-        },
-        (_, Some(("noop", _))) => UserCommand::Noop,
-        _ => UserCommand::UnknownCommand(cmd_str.clone())
-    }
-}
-
-fn find_with_vf_table(table_addr: *const *const UnknownType) {
-    let g_objects = GameBase::singleton().gobjects();
-
-    logln!("Finding objects with VFTable: {:p}", table_addr);
-    let mut count: usize = 0;
-
-    for i in 0..(g_objects.num_elements.to_native()-1) {
-        match g_objects.item_at_idx(i as usize) {
-            Some(item) => {
-                match item.object::<UObject>() {
-                    Some(obj) => {
-                        if obj.virtual_funcs() == table_addr {
-                            count += 1;
-                            logln!("GOBJECTS[{:?}]: {:?} ({:?})", i, obj.full_name(), obj.class().full_name());
-                        }
-                    }
-                    _ => ()
-                }
-                
-            }
-
-            None => ()
+        None => {
+            Err("No console attached to GameBase!".into())
         }
     }
+}
 
-    if count == 0 { logln!("No results found for VFTable: {:p}", table_addr); }
+fn call_intercept_shiv(shiv_fn: CommandInterceptShiv) -> impl Fn(*const UConsole, *const FString) {
+    return move |console: *const UConsole, cmd: *const FString| {
+        let console_ref = unsafe { console.as_ref::<'static>().unwrap() };
+        let cmd_ref = unsafe { cmd.as_ref::<'static>().unwrap() };
+
+        match shiv_fn(console_ref, cmd_ref) {
+            Ok(do_forward_cmd) => {
+                if do_forward_cmd { CommandIntercept.call(console, cmd) };
+            }
+
+            Err(err) => {
+                warning!("[{}]: Error intercepting console command: {:?}", GameBase::singleton().mod_name, err);
+                CommandIntercept.call(console, cmd)
+            }
+        }
+    }
 }
